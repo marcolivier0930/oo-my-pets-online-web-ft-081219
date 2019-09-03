d07f9f06053d834f58ae13530bf4a78955d7c9cd
Marc Blanchard <marcblanchard0930@gmail.com>
2019-09-03 00:31:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
-class Dog 
+class Dog
   
+  attr_accessor :owner, :mood
+  attr_reader :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
 end
