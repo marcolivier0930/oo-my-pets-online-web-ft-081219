d3f8e30369626fb5ce0b5d5ec5bf56e344eb1574
Marc Blanchard <marcblanchard0930@gmail.com>
2019-09-03 00:19:40 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,15 +1,24 @@
 class Owner 
   
   attr_reader :name 
-
+  
+  @@all = []
+  
   def initialize(name, species = "human")
     @name = name
     @species = species 
+    @@all << self 
   end
   
   def say_species
     return "I am a #{species}."
   end
   
+  def self.all 
+    @@all 
+  end
   
+  def self.count 
+    return @@all.count 
+  end
 end
