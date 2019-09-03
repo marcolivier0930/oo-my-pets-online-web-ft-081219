95800e2dbd3850d371745c1211b564fd6071fed1
Marc Blanchard <marcblanchard0930@gmail.com>
2019-09-03 00:28:51 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,8 +3,15 @@ class Cat
   attr_accessor :name, :mood
   attr_reader :mood 
   
+  @@all = []
+  
   def initialize(name)
     @name = name
     @mood = "nervous"
+    @@all 
+  end
+  
+  def self.all 
+    @@all
   end
 end
